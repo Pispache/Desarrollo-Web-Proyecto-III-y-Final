4e476a35bc43d9fa61ee251183334b1b1e20a955
CesarTecun <163237729+CesarTecun@users.noreply.github.com>
2025-09-14 23:10:40 -0600

    .

--- scripts/db-init.sh
+++ scripts/db-init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
-/opt/mssql-tools18/bin/sqlcmd -S db,1433 -C -U sa -P "$SA_PASSWORD" -i /db/init.sql
+/opt/mssql-tools18/bin/sqlcmd -S db,1435 -C -U sa -P "$SA_PASSWORD" -i /db/init.sql
 if [ -f /db/seed.sql ]; then
-  /opt/mssql-tools18/bin/sqlcmd -S db,1433 -C -U sa -P "$SA_PASSWORD" -i /db/seed.sql
+  /opt/mssql-tools18/bin/sqlcmd -S db,1435 -C -U sa -P "$SA_PASSWORD" -i /db/seed.sql
 fi
 echo "DB inicializada."
